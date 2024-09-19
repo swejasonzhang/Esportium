47b6c469d3325f7dee4cdb25e862dc980526962b
swejasonzhang <swejasonzhang@gmail.com>
2024-09-18 23:14:19 -0400

    created userschema for mongodb

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import colors from "colors"
 
 const connectDB = async () => {
   try {
     const con = await mongoose.connect("mongodb://localhost:27017/Esportium");
-    console.log(`Connected to MongoDB ${con.connection.host}`);
+    console.log(`Connected to MongoDB ${con.connection.host}`.bgBlue);
   } catch (error) {
     console.log(`MongoDB Connection Error - ${error} `);
   }
