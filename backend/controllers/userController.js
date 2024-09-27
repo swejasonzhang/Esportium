08956f5532f40e8b98385e5be0104b7ff1e52251
swejasonzhang <swejasonzhang@gmail.com>
2024-09-26 22:35:21 -0400

    implemented error handling if name email or password dont exist

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,15 @@
-const registerController = (req, res) => {
-  res.send("Register Controller Working");
+import userModel from "./models/userModel.js";
+
+const registerController = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if ((!name, !email, !password)) {
+    return res
+      .status(400)
+      .send({ sucess: false, message: "All fields are required" });
+  }
+
+  const newUser = await userModel.create({ name, email, password });
 };
 
 export { registerController };
