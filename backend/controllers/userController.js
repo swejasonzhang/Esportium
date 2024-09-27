e20d0e51dc32318811f58d9d64c21c7ea98a1e6f
swejasonzhang <swejasonzhang@gmail.com>
2024-09-26 22:41:21 -0400

    checking if the email already exist

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,33 @@
 import userModel from "./models/userModel.js";
 
 const registerController = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  if ((!name, !email, !password)) {
+    if ((!name, !email, !password)) {
+      return res
+        .status(400)
+        .send({ sucess: false, message: "All fields are required" });
+    }
+
+    const isExist = await userModel.findOne({ email });
+    if (isExist) {
+      return res
+        .status(400)
+        .send({ success: false, message: "Email already exist" });
+    }
+
+    const newUser = await userModel.create({ name, email, password });
+
+    return res
+      .status(201)
+      .send({ sucess: true, message: "User registration sucessful", newUser });
+  } catch (error) {
+    console.log(`registerController Error ${error}`);
     return res
       .status(400)
-      .send({ sucess: false, message: "All fields are required" });
+      .send({ success: false, message: "error in registerController", error });
   }
-
-  const newUser = await userModel.create({ name, email, password });
 };
 
 export { registerController };
