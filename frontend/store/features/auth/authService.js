48908ceb8a44238312ae24f2acf77b49a3fdc1f7
swejasonzhang <swejasonzhang@gmail.com>
2024-10-10 23:35:33 -0400

    created auth service file with an function to login an user using axios, implemented that function within the auth slice and auth slice within the login page

---
+++ frontend/store/features/auth/authService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const loginUser = async (userData) => {
+  const axiosResponse = axios
+    .post(`${process.env.NEXT_PUBLIC_BASE_URL}/users/login`, userData, {
+      withCredentials: true,
+      headers: { "Content-Type": "application/json" },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      return error.response.data;
+    });
+  return axiosResponse;
+};
+
+const authService = { loginUser };
+export default authService;
--- frontend/store/features/auth/authSlice.js
+++ frontend/store/features/auth/authSlice.js
@@ -1,6 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
+import authService from "./authService";
 
-const initialState = null;
+const initialState = {
+  user: null,
+  status: "idle",
+  error: null,
+};
+
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  try {
+    return await authService.loginUser(user);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
 
 export const authSlice = createSlice({
   name: "auth",
