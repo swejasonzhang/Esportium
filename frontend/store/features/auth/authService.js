8891b5b11c1e4dd6b785430923707f69c5aca5c7
swejasonzhang <swejasonzhang@gmail.com>
2024-10-31 23:51:42 -0400

    added logout user function within authService

--- frontend/store/features/auth/authService.js
+++ frontend/store/features/auth/authService.js
@@ -40,5 +40,24 @@ const loginUser = async (inputValues) => {
   }
 };
 
-const authService = { loginUser, registerUser };
+const logoutUser = async () => {
+  try {
+    const axiosResponse = await axios.get(
+      `${process.env.NEXT_PUBLIC_URL_BASE}/users/logout`,
+      {
+        withCredentials: true,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    return axiosResponse.data;
+  } catch (error) {
+    const errorMessage =
+      error.message?.data?.message ||
+      error.message ||
+      "Something went wrong! Please try again.";
+    return Promise.reject(errorMessage);
+  }
+};
+
+const authService = { loginUser, registerUser, logoutUser };
 export default authService;
