7915442f0b4621409121000f7e30b27e0488c924
swejasonzhang <swejasonzhang@gmail.com>
2024-11-11 22:57:47 -0500

    added a counter to represent item count

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { CircleUser, Menu, Package2, Search, Sheet } from "lucide-react";
+import { CircleUser, Menu, Package2, Sheet } from "lucide-react";
 import { SheetContent, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
-import { Input } from "./ui/input";
 import { DropdownMenu, DropdownMenuContent } from "./ui/dropdown-menu";
 import {
   DropdownMenuItem,
@@ -101,16 +100,9 @@ function NavBar() {
             </nav>
           </SheetContent>
         </Sheet>
-        <div className="flex w-full items-center gap-4 md:ml-auto md:gap lg:gap-4">
-          <form className="ml-auto flex-1 sm:flex-initial">
-            <div className="relative">
-              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type="search"
-                placeholder="Search Products..."
-                className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-              />
-            </div>
+        <div className="flex w-full items-center gap-4 ml-auto md:gap-2 lg:gap-4">
+          <form className="ml-auto  sm:flex-initial">
+            <div className="relative">Cart (0)</div>
           </form>
           <DropdownMenu>
             <DropdownMenuTrigger asChildsChild>
