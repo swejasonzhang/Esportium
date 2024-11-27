d94093c8c5601e345a91c61cc9e114f84b8a0396
swejasonzhang <swejasonzhang@gmail.com>
2024-11-27 00:11:54 -0500

    changed catagories page to have cards

--- frontend/pages/catagories.js
+++ frontend/pages/catagories.js
@@ -1,9 +1,35 @@
-import { React } from 'react';
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "@components/ui/card";
+import { Input } from "@/components/ui/input";
 
 function Catagories() {
-    return (
-        <div>Catagories</div>
-    )
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Dashboard</CardTitle>
+          <CardDescription>
+            Used to identify your store in the marketplace.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <Input placeholder="Dashboard" />
+          </form>
+        </CardContent>
+        <CardFooter className="border-t px-6 py-4">
+          <Button>Save</Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
 }
 
 export default Catagories;
